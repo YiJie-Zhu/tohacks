0e4bdd86001b5ce165f796e41c21893f61ca39de
Tony Huang <t.tonyhuang@gmail.com>
2020-05-03 02:15:57 -0400

    fix reddit

--- reddit.js
+++ reddit.js
@@ -1,34 +1,39 @@
+
+
 var contains = false
 
 function replaceText(element) {
-  if (element.hasChildNodes()) {
-    element.childNodes.forEach(replaceText)
-  } else if (element.nodeType === Text.TEXT_NODE) {
-    if (element.textContent.match(/coronavirus/gi) || element.textContent.match(/covid-19/gi) || 
-    element.textContent.match(/covid/gi)) {
-        contains = true;
+    if (element.hasChildNodes()) {
+      element.childNodes.forEach(replaceText)
+    } else if (element.nodeType === Text.TEXT_NODE) {
+      if (element.textContent.match(/coronavirus/gi) || element.textContent.match(/covid-19/gi) || 
+      element.textContent.match(/covid/gi)) {
+          contains = true;
+      }
     }
   }
-}
 
-var list = document.getElementsByClassName("_1poyrkZ7g36PawDueRza-J");
-for (var i = 0; i < list.length; i++) {
-  // this will remove the node from the pageif (list[i].hasChildNodes()) {}
-  replaceText(list[i])
-  if (contains) {
-      list[i].style.display = "none";
+
+function replaceReddit(){
+    var list = document.getElementsByClassName("_2XDITKxlj4y3M99thqyCsO");
+        for (var i = 0; i < list.length; i++) {
+        replaceText(list[i])
+        if (contains){
+        list[i].style.display = "none";
+        }
+        contains = false;
+    }
+
+  var list = document.getElementsByClassName("yn9v_hQEhjlRNZI0xspbA");
+  for (var i = 0; i < list.length; i++) {
+      replaceText(list[i])
+      if (contains){
+          list[i].style.display = "none";
+        }
+    contains = false;
   }
-  contains = false; 
 }
 
+window.setInterval(replaceReddit, 1000);
 
-var list2 = document.getElementsByClassName("_1poyrkZ7g36PawDueRza-J");
-for (var i = 0; i < list2.length; i++) {
-  // this will remove the node from the pageif (list[i].hasChildNodes()) {}
-  replaceText(list2[i])
-  if (contains) {
-      list2[i].style.display = "none";
-  }
-  contains = false; 
-}
 
